5649c23950fdddddae50be37d10e67faacd7e677
zhangwen666666 <1813505899@qq.com>
2024-08-01 21:28:27 +0800

    Vue

---
+++ Vue/code/js/myvue.js
@@ -0,0 +1,20 @@
+//定义一个Vue类
+class Vue{
+    //定义构造函数
+    //options是一个简单的纯粹的JS对象：{}
+    constructor(options){
+        //获取所有的属性名
+        Object.keys(options.data).forEach((propertyName,index) =>{
+            // console.log(propertyName,index);
+            Object.defineProperty(this,propertyName,{
+                get(){
+                    return options.data[propertyName];
+                },
+                set(val){
+                    options.data[propertyName] = val;
+                }
+            })
+        })
+        // Object.defineProperty(this,'')
+    }
+}
