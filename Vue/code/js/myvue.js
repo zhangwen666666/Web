8fe12aa5304f929f58bf38758a48113ed6251c95
zhangwen666666 <1813505899@qq.com>
2024-08-20 19:06:48 +0800

    Vue

--- Vue/code/js/myvue.js
+++ Vue/code/js/myvue.js
@@ -9,11 +9,17 @@ class Vue {
             let firstChar = propertyName.charAt(0);
             if (firstChar != '_' && firstChar != '$') {
                 Object.defineProperty(this, propertyName, {
+                    // 数据代理
                     get() {
                         return options.data[propertyName];
                     },
+
+                    // 数据劫持
                     set(val) {
+                        //1. 修改内存中改对象的属性值
                         options.data[propertyName] = val;
+                        //2. 重新渲染页面
+                        console.log('重新渲染页面')
                     }
                 })
             }
@@ -21,10 +27,10 @@ class Vue {
         // Object.defineProperty(this,'')
 
         //获取所有的方法名
-        Object.keys(options.methods).forEach((methodName, index) => {
-            // console.log(methodName);
-            // 给当前的Vue实例扩展一个方法
-            this[methodName] = options.methods[methodName];
-        })
+        // Object.keys(options.methods).forEach((methodName, index) => {
+        //     // console.log(methodName);
+        //     // 给当前的Vue实例扩展一个方法
+        //     this[methodName] = options.methods[methodName];
+        // })
     }
 }
