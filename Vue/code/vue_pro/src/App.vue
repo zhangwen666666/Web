e115ab159cccf0b16d1290df876884447719392d
zhangwen666666 <1813505899@qq.com>
2024-08-25 17:50:52 +0800

    Vue

--- Vue/code/vue_pro/src/App.vue
+++ Vue/code/vue_pro/src/App.vue
@@ -1,16 +1,33 @@
 <template>
     <div>
-        <A/>
-        <hr>
-        <B/>
+        <div>
+            <h1>省份</h1>
+            <ul>
+                <li><a href="./hebei.html" class="selected">河北省</a></li>
+                <li><a href="./henan.html">河南省</a></li>
+            </ul>
+        </div>
+
+        <!-- 这个位置将来是要显示对应的组件的 -->
+        <!-- <HeBei></HeBei>
+        <HeNan></HeNan> -->
     </div>
 </template>
 
 <script>
-    import A from './components/A.vue'
-    import B from './components/B.vue'
+/*     import HeBei from './components/hebei.vue'
+    import HeNan from './components/henan.vue' */
     export default {
         name: 'App',
-        components:{A, B}
+        // components: {HeBei,HeNan}
     }
 </script>
+
+<style lang="css">
+    .s1{
+        margin-left: 100px;
+    }
+    .selected{
+        background-color: aqua;
+    }
+</style>
--- Vue/code/vue_pro/src/main.js
+++ Vue/code/vue_pro/src/main.js
@@ -6,10 +6,17 @@ import App from './App.vue'
 // 关闭生产提示信息
 Vue.config.productionTip = false
 
-import store from './vuex/store'
+// 导入vue-router插件
+import VueRouter from 'vue-router'
+// 导入路由器对象
+
+// 使用vue-router插件
+Vue.use(VueRouter)
 
 const vm = new Vue({
   el: '#app',
-  store,
+  // 一但使用了vue-router插件，那么new Vue的时候就可以直接传递一个配置项:router
+  // router配置项用来接收一个路由器对象
+
   render: h => h(App),
 })
