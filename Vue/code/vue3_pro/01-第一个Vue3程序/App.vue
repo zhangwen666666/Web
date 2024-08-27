409b1f8136f382a267af15a7ee4cceba9cfeb097
zhangwen666666 <1813505899@qq.com>
2024-08-27 21:43:17 +0800

    Vue

---
+++ Vue/code/vue3_pro/01-第一个Vue3程序/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<h1>欢迎大家学习Vue3</h1>
+	<HelloWorld/>
+</template>
+
+<script>
+	import HelloWorld from './components/HelloWorld.vue'
+	export default {
+		name: 'App',
+		components:{HelloWorld}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/code/vue3_pro/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2>大家好</h2>
+    <h2>我是第二个根标签</h2>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld'
+    }
+</script>
+
+<style>
+
+</style>
