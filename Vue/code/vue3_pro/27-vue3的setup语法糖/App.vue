4244907e230550fb256a3ee49cbbdf467c5df6b4
zhangwen666666 <1813505899@qq.com>
2024-09-04 21:38:07 +0800

    Vue

---
+++ Vue/code/vue3_pro/27-vue3的setup语法糖/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<h2>计数器:  {{counter}}</h2>
+	<button @click="add">计数器加1</button>
+	<User/>
+</template>
+
+
+<script setup>
+	// 语法糖：在script标签中加一个setup
+	// 这个语法糖会自动将script标签体中定义的一级的数据，函数，计算属性...交出去
+	// 组件也不需要注册了，只需要引入即可
+	import { ref } from "vue";
+	import User from './components/User.vue'
+
+	// data
+	let counter = ref(100)
+	// methods
+	function add(){
+		counter.value++;
+	}
+	// watch
+	// computed
+	// ...
+
+	// export default {
+	// 	name: 'App',
+	// 	setup(){
+	// 		// data
+	// 		let counter = ref(100)
+
+	// 		// methods
+	// 		function add(){
+	// 			counter.value++;
+	// 		}
+	// 		// watch
+	// 		// computed
+	// 		// ...
+
+	// 		// 返回一个对象
+	// 		return {counter, add}
+	// 	}
+	// }
+</script>
+
---
+++ Vue/code/vue3_pro/27-vue3的setup语法糖/components/User.vue
@@ -0,0 +1,7 @@
+<template>
+    <h2>User组件</h2>
+</template>
+
+<script setup>
+
+</script>
