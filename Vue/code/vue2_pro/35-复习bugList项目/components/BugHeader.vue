24e0e5f1d81a0b53ae36098f22704270f1b30971
zhangwen666666 <1813505899@qq.com>
2024-09-08 21:54:50 +0800

    Vue

---
+++ Vue/code/vue2_pro/35-复习bugList项目/components/BugHeader.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <textarea cols="105" rows="4" placeholder="请输入BUG的描述信息" v-model.lazy.trim="desc"></textarea>
+        <!-- {{desc}} -->
+        <br>
+        <button class="small green button" @click="save">保存</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'BugHeader',
+        // props:['saveBugCallBack'],
+        data(){
+            return {
+                desc:''
+            }
+        },
+        methods: {
+            save(){
+                if(this.desc === '') return
+                // this.saveBugCallBack({id: Date.now(), desc: this.desc, selected: false});
+                
+                // 触发事件
+                this.$emit('saveBugCallBack', {id: Date.now(), desc: this.desc, selected: false})
+                this.desc = ''
+            }
+        }
+    }
+</script>
+
+<style>
+    .header {
+        margin-bottom: 20px;
+        margin-top: 20px;
+    }
+</style>
